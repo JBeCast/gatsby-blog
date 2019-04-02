ee7b9afe3c10363c6d8d3145bae3cb55ab131c04
Joaquín Bermúdez Castanheira <jbcsound@gmail.com>
2019-04-02 15:22:55 +0200

    Add a third sample page.

--- src/pages/page-3.jsx
+++ src/pages/page-3.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link, StaticQuery } from "gatsby"
 
 import Layout from '../components/layout';
+
+const getImageData = graphql`
+  {
+    allFile {
+      edges {
+        node {
+          relativePath
+          size
+          extension
+          birthTime
+        }
+      }
+    }
+  }
+`
 
 export default () => (
   <Layout>
     <h1>Hello from page 3!</h1>
+    <StaticQuery
+      query={getImageData}
+      render={data => (
+        <table>
+          <thead>
+            <tr>
+              <th>Relative Path</th>
+              <th>Size</th>
+              <th>Extension</th>
+              <th>BirthTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({node}, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.size}</td>
+                <td>{node.extension}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    />
     <Link to="/page-2">Go to page 2</Link>
   </Layout>
 )
